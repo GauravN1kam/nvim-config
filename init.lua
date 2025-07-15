ad27434bd5865b15b958f667b2ca9dd45761d14d
Gaurav Nikam <gauravpn.a2@gmail.com>
2025-07-15 18:08:33 +0530

    changes in init.lua

--- init.lua
+++ init.lua
@@ -13,14 +13,29 @@ vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
+vim.keymap.set("n", "<space><space>x", ":source %<CR>")
+
 vim.keymap.set("n", "<space>to", ":tabnew<CR>")   -- open new tab
 vim.keymap.set("n", "<space>tx", ":tabclose<CR>") -- close current tab
 vim.keymap.set("n", "<space>tn", ":tabn<CR>")     --  go to next tab
 vim.keymap.set("n", "<space>tp", ":tabp<CR>")     --  go to previous tab
 
+vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "None" })
 
+function ToggleBackground()
+  if toggleBack == false then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  end
+  if toggleBack == true then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
+  end
+  toggleBack = not toggleBack
+  print(toggleBack)
+end
 
--- vim.diagnostic.config({
 --   virtual_text = {
 --     prefix = "●", -- or "■", "▶", "", etc.
 --     spacing = 4,
